6296be3dea338e8b8c390d5ce487524d0a44cfb1
volard <kirillzhilenkov@gmail.com>
2024-02-15 04:30:49 +0800

    feat: add simple one-client web socket endpoint

--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,13 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.WebHost.UseUrls("http://localhost:6969");
 
 var app = builder.Build();
 
@@ -11,6 +17,36 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseWebSockets();
 
-app.Run();
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+        while (true)
+        {
+            var message = "Time is " + DateTime.Now.ToString("HH:mm:ss");
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            if (ws.State == WebSocketState.Open)
+            {
+                await ws.SendAsync(arraySegment,
+                                WebSocketMessageType.Text,
+                                true,
+                                CancellationToken.None);
+            }
+            else if (ws.State is WebSocketState.Closed or WebSocketState.Aborted)
+            {
+                break;
+            }
+            Thread.Sleep(2000);
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
+
+app.RunAsync();
