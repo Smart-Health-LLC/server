4357017f575b31fdbbab9b62940fff0b9271014c
volard <kirillzhilenkov@gmail.com>
2024-04-25 03:28:47 +0800

    :fire: refactor: remove dumb repository

--- server/DataAccess/Repositories/BaseScheduleRepository.cs
+++
@@ -1,9 +0,0 @@
-using server.DataAccess.Models;
-
-namespace server.DataAccess.Repositories;
-
-public class BaseScheduleRepository(DatabaseContext applicationDbContext)
-    : Repository<BaseSchedule>(applicationDbContext)
-{
-    private readonly DatabaseContext _applicationDbContext = applicationDbContext;
-}
