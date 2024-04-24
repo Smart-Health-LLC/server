e95be6876fd0ef0152d28ea002c313fae79a98df
volard <kirillzhilenkov@gmail.com>
2024-04-24 18:11:34 +0800

    :coffin: refactor: remove business layer No need because of VSA (vertical slice architecture) usage

--- server/Business/Services/Interfaces/IReadServiceAsync.cs
+++
@@ -1,9 +0,0 @@
-namespace server.Business.Services.Interfaces;
-
-public interface IReadServiceAsync<TEntity, TDto>
-    where TEntity : class
-    where TDto : class
-{
-    Task<IEnumerable<TDto>> GetAllAsync();
-    Task<TDto> GetByIdAsync(int id);
-}
