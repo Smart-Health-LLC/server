b67b563c93ed0a3aebc47458de9cbea32dd2c1d1
volard <kirillzhilenkov@gmail.com>
2024-04-25 03:32:03 +0800

    :bug: fix: generic repository

--- server/DataAccess/Repositories/Repository.cs
+++ server/DataAccess/Repositories/Repository.cs
@@ -12,7 +12,7 @@ public class Repository<T> : IRepository<T> where T : class
     private readonly DatabaseContext _applicationDbContext;
     private readonly DbSet<T> _dbSet;
 
-    protected Repository(DatabaseContext applicationDbContext)
+    public Repository(DatabaseContext applicationDbContext)
     {
         _applicationDbContext = applicationDbContext;
         _dbSet = _applicationDbContext.Set<T>();
@@ -35,23 +35,23 @@ public class Repository<T> : IRepository<T> where T : class
         await _applicationDbContext.SaveChangesAsync();
     }
 
-    public async Task<T?> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+    public async Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
     {
         IQueryable<T?> query = _dbSet;
 
         if (!tracked) query = query.AsNoTracking();
 
         if (filter != null) query = query.Where(filter!);
-        return await query.FirstOrDefaultAsync(); // query will be executed here, deferred execution
+        return await query.FirstOrDefaultAsync();
     }
 
-    public async Task<List<T?>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+    public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
     {
         IQueryable<T?> query = _dbSet;
 
         if (!tracked) query = query.AsNoTracking();
 
         if (filter != null) query = query.Where(filter!);
-        return await query.ToListAsync(); // query will be executed here, deferred execution
+        return await query.ToListAsync();
     }
 }
