e0b0c5de2f97f4ae425619cd189372de24625c7a
volard <kirillzhilenkov@gmail.com>
2024-04-26 02:06:16 +0800

    :card_file_box: refactor: exclude ScheduleFamily description from db

--- server/DataAccess/Models/BaseScheduleFamily.cs
+++ server/DataAccess/Models/BaseScheduleFamily.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.DataAccess.Models;
 
 public class BaseScheduleFamily
 {
     public int Id { get; set; }
+
     [MaxLength(100)] public string Name { get; set; }
-    [MaxLength(1000)] public string? Description { get; set; }
+
+    // localized value is stored in resource files on the server separately
+    [NotMapped] public string? Description { get; set; }
 
     public virtual List<BaseSchedule>? Schedules { get; set; }
 }
