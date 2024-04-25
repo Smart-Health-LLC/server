ebdb7b80b0eec5bd3931e28bdaad6ae4be29857e
volard <kirillzhilenkov@gmail.com>
2024-04-26 02:26:57 +0800

    :recycle: refactor: separate seed logic

--- server/DataAccess/DatabaseContext.cs
+++ server/DataAccess/DatabaseContext.cs
@@ -31,57 +31,6 @@ public class DatabaseContext(IOptions<DbSettings> dbSettings) : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
-        // TODO add base schedule data and other predefined stuff
-        modelBuilder.Entity<BaseScheduleFamily>().HasData(
-            new BaseScheduleFamily
-            {
-                Id = 1,
-                Name = "Biphasic",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 2,
-                Name = "Everyman",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 3,
-                Name = "Dual core",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 4,
-                Name = "Tri core",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 5,
-                Name = "Core-only",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 6,
-                Name = "Nap-only",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 7,
-                Name = "Flexible",
-                Schedules = []
-            },
-            new BaseScheduleFamily
-            {
-                Id = 8,
-                Name = "Non-reducing",
-                Schedules = []
-            }
-        );
+        modelBuilder.Seed();
     }
 }
